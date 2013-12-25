b7a9d63a3af6ec39c837fb62f279e48a2975ea58
ditofry <brandon@gocirro.com>
2013-12-25 02:39:17 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LaurensSite::Application.config.secret_key_base = '56978030adb33bbac1c19dd4a41781067e7c6d35811fe51fbcc00e2affca503d3a0cffadf25c3272ac98aa1a96196c005f56f088b3c81c0f47fae6194050ccba'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LaurensSite::Application.config.session_store :cookie_store, key: '_laurens_site_session'
